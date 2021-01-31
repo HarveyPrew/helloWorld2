81d905b485fa189d96241d4c0a8fdc64fdfb2ef7
HarveyPrew <harvey.prewer@gmail.com>
2021-01-31 18:40:56 +0000

    Initial Commit

---
+++ helloWorld2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  helloWorld2
+//
+//  Created by Harvey on 31/01/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
